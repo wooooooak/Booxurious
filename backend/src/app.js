21cdcfd2061c921ee8ffec367d3e8c7dfb811271
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-09-17 15:29:07 +0900

    [#20] 운영환경 이미지 업로드 에러 해결

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,20 +10,41 @@ import { sync } from './db/sync';
 
 const app = express();
 app.use(cors());
-api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({ extended: true }));
-api.use(awsServerlessExpressMiddleware.eventContext());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 sequelize.authenticate().then(() => {
   sync();
 });
 
 app.use('/', api);
+
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+];
+
 if (process.env.NODE_ENV === 'development') {
   app.listen(8080, () => {
     console.log(`Express server listening on port 8080`);
   });
 }
-const server = awsServerlessExpress.createServer(app);
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);

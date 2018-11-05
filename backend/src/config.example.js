8f2e553bbee413bf37110db191a2eb1a30656f9a
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-11-05 15:02:38 +0900

    설정 example 파일 추가

---
+++ backend/src/config.example.js
@@ -0,0 +1,18 @@
+export const config = {
+  db_config_local: {
+    database_name: "booxurious",
+    username: "your-name",
+    password: "your-password",
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306
+  },
+  db_config_aws: {
+    database_name: "booxurious",
+    username: "your-name",
+    password: "0000",
+    dialect: "your-password",
+    host: "",
+    port: 3306
+  }
+};
---
+++ backend/src/secret.example.js
@@ -0,0 +1,1 @@
+export const jwtSecretKey = "secret-key";

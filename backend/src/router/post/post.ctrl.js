4f8cfdf4a6e197c98ba52c174ae13151feb794da
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-11-24 00:27:02 +0900

    글쓰기 오류 수정 - post.setUser(user) 를 하면 에러가 났는데 post.setUser(fk_user_id)로 해결. - 다시 봐야될 듯

--- backend/src/router/post/post.ctrl.js
+++ backend/src/router/post/post.ctrl.js
@@ -1,46 +1,47 @@
-import Post from "../../db/model/Post";
-import User from "../../db/model/User";
-import Category from "../../db/model/Category";
+import Post from '../../db/model/Post';
+import User from '../../db/model/User';
+import Category from '../../db/model/Category';
 
 export const uploadBookCoverImage = (req, res) => {
-  let imgFile = req.file;
-  res.json(imgFile);
+	let imgFile = req.file;
+	res.json(imgFile);
 };
 
 export const uploadImageInContent = (req, res) => {
-  let imgFile = req.file;
-  res.status(200).json(imgFile);
+	let imgFile = req.file;
+	res.status(200).json(imgFile);
 };
 
 export const write = async (req, res) => {
-  const { category } = req.body;
-  const { userId: fk_user_id } = req.decodedUser;
-  try {
-    const machedCategory = await Category.findOrCreate({ where: { name: category } });
-    const user = await User.find({ where: { id: fk_user_id } });
-    const post = await Post.create(req.body);
-    await post.setUser(user);
-    await post.setCategory(machedCategory[0]);
-    post.save();
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
-
-export const test = (req, res) => {
-  res.status(200).json({
-    key: process.env.AWS_KEY
-  });
+	const { category } = req.body;
+	const { userId: fk_user_id } = req.decodedUser;
+	try {
+		const machedCategory = await Category.findOrCreate({
+			where: { name: category }
+		});
+		const user = await User.find({ where: { id: fk_user_id } });
+		const post = await Post.create(req.body);
+		// await post.setUser(user);
+		await post.setUser(fk_user_id);
+		await post.setCategory(machedCategory[0]);
+		await post.save();
+		res.status(200).json(post);
+	} catch (error) {
+		// console.log(error);
+		res.status(500).json(error);
+	}
 };
 
 export const getPostsByUserId = async (req, res) => {
-  const { userId } = req.query;
-  const posts = await Post.findAll({ where: { fk_user_id: userId }, order: [ [ "createdAt", "DESC" ] ] });
-  res.status(200).json(posts);
+	const { userId } = req.query;
+	const posts = await Post.findAll({
+		where: { fk_user_id: userId },
+		order: [ [ 'createdAt', 'DESC' ] ]
+	});
+	res.status(200).json(posts);
 };
 export const getPostByPostId = async (req, res) => {
-  const { postId } = req.query;
-  const posts = await Post.findOne({ where: { id: postId } });
-  res.status(200).json(posts);
+	const { postId } = req.query;
+	const posts = await Post.findOne({ where: { id: postId } });
+	res.status(200).json(posts);
 };
--- frontend/src/component/Write/ImageUploader.tsx
+++ frontend/src/component/Write/ImageUploader.tsx
@@ -1,22 +1,22 @@
-import * as React from "react";
-import styled, { keyframes } from "styled-components";
-import styledTS from "styled-components-ts";
-import { Upload } from "styled-icons/feather/Upload";
-import { Loader } from "styled-icons/feather/Loader";
+import * as React from 'react';
+import styled, { keyframes } from 'styled-components';
+import styledTS from 'styled-components-ts';
+import { Upload } from 'styled-icons/feather/Upload';
+import { Loader } from 'styled-icons/feather/Loader';
 
 const Conatiner = styled.div`
-  width: 180px;
-  height: 280px;
-  margin-left: auto;
+	width: 180px;
+	height: 280px;
+	margin-left: auto;
 `;
 
 interface ImgProps {
-  bookCoverImge: string | null;
+	bookCoverImge: string | null;
 }
 
 const Img = styledTS<ImgProps>(styled.div)`
   position: absolute;
-  background-color: ${(props) => (props.bookCoverImge ? null : "transparent")};
+  background-color: ${(props) => (props.bookCoverImge ? null : 'transparent')};
   background-image: url('${(props) => props.bookCoverImge}');
   background-repeat: no-repeat;
   background-size: cover;
@@ -24,8 +24,9 @@ const Img = styledTS<ImgProps>(styled.div)`
   width: 180px;
   /* margin-left: auto; */
   border-radius: 10px;
-  box-shadow: ${(props) => (props.bookCoverImge ? "5px 5px 5px grey" : "transparent")};
-  cursor: ${(props) => (props.bookCoverImge ? "pointer" : null)};
+  box-shadow: ${(props) =>
+		props.bookCoverImge ? '5px 5px 5px grey' : 'transparent'};
+  cursor: ${(props) => (props.bookCoverImge ? 'pointer' : null)};
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -33,7 +34,7 @@ const Img = styledTS<ImgProps>(styled.div)`
 `;
 
 const FileUploaderButton = styled.input.attrs({
-  type: "file"
+	type: 'file'
 })`
   position: absolute;
   height: 280px;
@@ -56,38 +57,44 @@ const lodingAnimation = keyframes`
 const Loding = Loader.extend`animation: ${lodingAnimation} 2s linear infinite;`;
 
 interface Props {
-  type: "write" | "read";
-  bookCoverImg: string | null;
-  uploadingImg: boolean;
-  fileChangedHandler?(e: FileList | null): void;
+	type: 'write' | 'read';
+	bookCoverImg: string | null;
+	uploadingImg: boolean;
+	fileChangedHandler?(e: FileList | null): void;
 }
 
-const ImageUploader: React.SFC<Props> = ({ bookCoverImg, uploadingImg, fileChangedHandler, type }) => {
-  console.log(bookCoverImg);
-  return (
-    <Conatiner>
-      <Img bookCoverImge={bookCoverImg}>
-        {uploadingImg ? (
-          <React.Fragment>
-            <Loding size={70} />
-            <p>이미지 등록 중!</p>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            {type === "write" ? (
-              <React.Fragment>
-                <Upload size={40} />
-                <p>Book Cover Image</p>
-              </React.Fragment>
-            ) : null}
-          </React.Fragment>
-        )}
-      </Img>
-      {fileChangedHandler ? (
-        <FileUploaderButton onChange={(e) => fileChangedHandler(e.target.files)} />
-      ) : null}
-    </Conatiner>
-  );
+const ImageUploader: React.SFC<Props> = ({
+	bookCoverImg,
+	uploadingImg,
+	fileChangedHandler,
+	type
+}) => {
+	return (
+		<Conatiner>
+			<Img bookCoverImge={bookCoverImg}>
+				{uploadingImg ? (
+					<React.Fragment>
+						<Loding size={70} />
+						<p>이미지 등록 중!</p>
+					</React.Fragment>
+				) : (
+					<React.Fragment>
+						{type === 'write' ? (
+							<React.Fragment>
+								<Upload size={40} />
+								<p>Book Cover Image</p>
+							</React.Fragment>
+						) : null}
+					</React.Fragment>
+				)}
+			</Img>
+			{fileChangedHandler ? (
+				<FileUploaderButton
+					onChange={(e) => fileChangedHandler(e.target.files)}
+				/>
+			) : null}
+		</Conatiner>
+	);
 };
 
 export default ImageUploader;

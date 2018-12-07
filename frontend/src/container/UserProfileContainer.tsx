53d640f2c8d6e98675d441c685869c171710c0b1
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-12-07 20:17:56 +0900

    수정 버튼 대신 팔로잉이 보이는 부분 수정
    -로그인시 id값을 받아오지 못한 점을 고침

--- frontend/src/container/UserProfileContainer.tsx
+++ frontend/src/container/UserProfileContainer.tsx
@@ -1,163 +1,163 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import { IStoreState } from "../store/modules";
+import { IStoreState } from '../store/modules';
 // import { IUserState } from "../store/modules/User";
-import { actionCreators as userActionCreator } from "../store/modules/User";
+import { actionCreators as userActionCreator } from '../store/modules/User';
 
-import InputForm from "../component/Profile/InputForm";
-import Modal from "../component/Profile/Modal";
-import ProfileWrapper from "../component/Profile/ProfileWrapper";
+import InputForm from '../component/Profile/InputForm';
+import Modal from '../component/Profile/Modal';
+import ProfileWrapper from '../component/Profile/ProfileWrapper';
 
-import { PostState } from "../store/modules/Post";
-import { WorkState } from "../store/modules/Work";
-import axios from "axios";
+import { PostState } from '../store/modules/Post';
+import { WorkState } from '../store/modules/Work';
+import axios from 'axios';
 
 interface StoreProps {
-  id: string;
-  email: string;
-  username: string | null;
-  profileImg: string;
+	id: string;
+	email: string;
+	username: string | null;
+	profileImg: string;
 }
 
 interface DispatchProps {
-  userAction: typeof userActionCreator;
+	userAction: typeof userActionCreator;
 }
 
 interface OwnProps {
-  matchedName: string;
+	matchedName: string;
 }
 
 type Props = StoreProps & DispatchProps & OwnProps;
 
 interface State {
-  userInfo: {
-    id: string;
-    username: string;
-    followerCount: number;
-    profileImg: string;
-    email: string;
-    reviews: PostState[];
-    works: WorkState[];
-  };
-  modalVisible: boolean;
+	userInfo: {
+		id: string;
+		username: string;
+		followerCount: number;
+		profileImg: string;
+		email: string;
+		reviews: PostState[];
+		works: WorkState[];
+	};
+	modalVisible: boolean;
 }
 
 class UserProfileContainer extends React.Component<Props, State> {
-  state = {
-    userInfo: {
-      id: "",
-      username: "",
-      followerCount: 0,
-      profileImg: "",
-      email: "",
-      reviews: [],
-      works: []
-    },
-    modalVisible: false
-  };
+	state = {
+		userInfo: {
+			id: '',
+			username: '',
+			followerCount: 0,
+			profileImg: '',
+			email: '',
+			reviews: [],
+			works: []
+		},
+		modalVisible: false
+	};
 
-  async componentDidMount () {
-    const { matchedName } = this.props;
-    try {
-      const { data } = await axios({
-        method: "get",
-        url: `${process.env.REACT_APP_DOMAIN}/user/${matchedName}`
-      });
-      const { id, username, email, profileImg } = data;
-      const postResult = await axios({
-        method: "get",
-        url: `${process.env.REACT_APP_DOMAIN}/post/user_id`,
-        params: { userId: id }
-      });
-      this.setState({
-        userInfo: {
-          ...this.state.userInfo,
-          id,
-          username,
-          email,
-          reviews: postResult.data,
-          profileImg
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async componentDidMount() {
+		const { matchedName } = this.props;
+		try {
+			const { data } = await axios({
+				method: 'get',
+				url: `${process.env.REACT_APP_DOMAIN}/user/${matchedName}`
+			});
+			const { id, username, email, profileImg } = data;
+			const postResult = await axios({
+				method: 'get',
+				url: `${process.env.REACT_APP_DOMAIN}/post/user_id`,
+				params: { userId: id }
+			});
+			this.setState({
+				userInfo: {
+					...this.state.userInfo,
+					id,
+					username,
+					email,
+					reviews: postResult.data,
+					profileImg
+				}
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  onClickSettingButton = (): void => {
-    this.setState({
-      modalVisible: true
-    });
-  };
+	onClickSettingButton = (): void => {
+		this.setState({
+			modalVisible: true
+		});
+	};
 
-  onClickSettingCancle = (): void => {
-    this.setState({
-      modalVisible: false
-    });
-  };
+	onClickSettingCancle = (): void => {
+		this.setState({
+			modalVisible: false
+		});
+	};
 
-  onClickModalOk = async (username: string) => {
-    try {
-      const token: string = localStorage.getItem("token") || "";
-      await axios({
-        method: "put",
-        url: `${process.env.REACT_APP_DOMAIN}/user`,
-        headers: { "Auth-Header": token },
-        data: { username }
-      });
-      this.setState({
-        userInfo: {
-          ...this.state.userInfo,
-          username
-        },
-        modalVisible: false
-      });
-      this.props.userAction.fetchUserData(token);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	onClickModalOk = async (username: string) => {
+		try {
+			const token: string = localStorage.getItem('token') || '';
+			await axios({
+				method: 'put',
+				url: `${process.env.REACT_APP_DOMAIN}/user`,
+				headers: { 'Auth-Header': token },
+				data: { username }
+			});
+			this.setState({
+				userInfo: {
+					...this.state.userInfo,
+					username
+				},
+				modalVisible: false
+			});
+			this.props.userAction.fetchUserData(token);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  render () {
-    console.log(this.state);
-    const { username, profileImg, reviews } = this.state.userInfo;
-    const { modalVisible } = this.state;
-    let Me = false;
-    if (this.props.id === this.state.userInfo.id) {
-      Me = true;
-    }
-    return (
-      <React.Fragment>
-        <InputForm
-          username={username}
-          profileImg={profileImg}
-          isMe={Me}
-          onClickSettingButton={this.onClickSettingButton}
-        />
-        <Modal
-          title="프로필 수정"
-          visible={modalVisible}
-          username={username}
-          onOk={this.onClickModalOk}
-          // confirmLoading={confirmLoading}
-          onCancel={this.onClickSettingCancle}
-        />
-        <ProfileWrapper postDatas={reviews} />
-      </React.Fragment>
-    );
-  }
+	render() {
+		const { username, profileImg, reviews } = this.state.userInfo;
+		const { modalVisible } = this.state;
+		let Me = false;
+		console.log(this.props);
+		if (this.props.id === this.state.userInfo.id) {
+			Me = true;
+		}
+		return (
+			<React.Fragment>
+				<InputForm
+					username={username}
+					profileImg={profileImg}
+					isMe={Me}
+					onClickSettingButton={this.onClickSettingButton}
+				/>
+				<Modal
+					title="프로필 수정"
+					visible={modalVisible}
+					username={username}
+					onOk={this.onClickModalOk}
+					// confirmLoading={confirmLoading}
+					onCancel={this.onClickSettingCancle}
+				/>
+				<ProfileWrapper postDatas={reviews} />
+			</React.Fragment>
+		);
+	}
 }
 
 export default connect<StoreProps, DispatchProps, OwnProps>(
-  ({ User }: IStoreState): StoreProps => ({
-    id: User.id,
-    email: User.email,
-    username: User.username,
-    profileImg: User.profileImg
-  }),
-  (dispatch: any) => ({
-    userAction: bindActionCreators(userActionCreator, dispatch)
-  })
+	({ User }: IStoreState): StoreProps => ({
+		id: User.id,
+		email: User.email,
+		username: User.username,
+		profileImg: User.profileImg
+	}),
+	(dispatch: any) => ({
+		userAction: bindActionCreators(userActionCreator, dispatch)
+	})
 )(UserProfileContainer);
--- frontend/src/store/modules/User.ts
+++ frontend/src/store/modules/User.ts
@@ -1,232 +1,261 @@
-import { createAction, handleActions } from "redux-actions";
-import axios from "axios";
+import { createAction, handleActions } from 'redux-actions';
+import axios from 'axios';
 
-const SOCIAL_LOGIN_SUCCESS = "user/SocailLogin";
-const SOCIAL_LOGIN_FAIL = "user/SocialLoginFail";
-const FETCH_USER_DATA_SUCCESS = "user/FetchUserDataSuccess";
-const FETCH_USER_DATA_FAIL = "user/FetchUserDataFail";
-const GO_TO_SIGN_IN_PAGE = "user/goToSignInPage";
-const SIGN_UP_SUCCESS = "user/signUpSuccess";
-const SIGN_UP_FAIL = "user/signUpFail";
-const LOGOUT = "user/logout";
+const SOCIAL_LOGIN_SUCCESS = 'user/SocailLogin';
+const SOCIAL_LOGIN_FAIL = 'user/SocialLoginFail';
+const FETCH_USER_DATA_SUCCESS = 'user/FetchUserDataSuccess';
+const FETCH_USER_DATA_FAIL = 'user/FetchUserDataFail';
+const GO_TO_SIGN_IN_PAGE = 'user/goToSignInPage';
+const SIGN_UP_SUCCESS = 'user/signUpSuccess';
+const SIGN_UP_FAIL = 'user/signUpFail';
+const LOGOUT = 'user/logout';
 
 const isUserExist = (result: any): boolean => {
-  return result.data.code === 1 ? true : false;
+	return result.data.code === 1 ? true : false;
 };
 
 export const fetchUserData = (token: string) => {
-  return async (dispatch: any) => {
-    try {
-      const { data } = await axios({
-        method: "get",
-        url: `${process.env.REACT_APP_DOMAIN}/user/token`,
-        headers: { "Auth-Header": token }
-      });
-      dispatch(
-        actionCreators.fetchUserDataSuccess({
-          ...data,
-          code: 200
-        })
-      );
-    } catch (error) {
-      alert("token 값이 만료되었습니다. 다시 로그인해주세요.");
-      dispatch(actionCreators.logout());
-    }
-  };
+	return async (dispatch: any) => {
+		try {
+			const { data } = await axios({
+				method: 'get',
+				url: `${process.env.REACT_APP_DOMAIN}/user/token`,
+				headers: { 'Auth-Header': token }
+			});
+			dispatch(
+				actionCreators.fetchUserDataSuccess({
+					...data,
+					code: 200
+				})
+			);
+		} catch (error) {
+			alert('token 값이 만료되었습니다. 다시 로그인해주세요.');
+			dispatch(actionCreators.logout());
+		}
+	};
 };
 
-export const socialLoginAsync = (socialEmail: string, profileImge: string, socialProvider: string) => {
-  return async (dispatch: any) => {
-    try {
-      const res = await axios({
-        method: "post",
-        url: `${process.env.REACT_APP_DOMAIN}/auth/login/social`,
-        data: {
-          email: socialEmail,
-          socialProvider
-        }
-      });
-      if (isUserExist(res)) {
-        localStorage.setItem("token", res.data.token);
-        dispatch(
-          actionCreators.socialLoginSuccess({
-            id: res.data.user.userId,
-            email: socialEmail,
-            profileImg: profileImge,
-            username: res.data.user.username,
-            socialProvider,
-            code: 200
-          })
-        );
-      } else {
-        dispatch(
-          actionCreators.socialLoginFail({
-            id: "",
-            socialProvider,
-            profileImg: profileImge,
-            email: res.data.email,
-            goToSignUpPage: true,
-            code: 500,
-            username: ""
-          })
-        );
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const socialLoginAsync = (
+	socialEmail: string,
+	profileImge: string,
+	socialProvider: string
+) => {
+	return async (dispatch: any) => {
+		try {
+			const { data } = await axios({
+				method: 'post',
+				url: `${process.env.REACT_APP_DOMAIN}/auth/login/social`,
+				data: {
+					email: socialEmail,
+					socialProvider
+				}
+			});
+			if (isUserExist(data)) {
+				localStorage.setItem('token', data.token);
+				dispatch(
+					actionCreators.socialLoginSuccess({
+						id: data.user.id,
+						email: socialEmail,
+						profileImg: profileImge,
+						username: data.user.username,
+						socialProvider,
+						code: 200
+					})
+				);
+			} else {
+				dispatch(
+					actionCreators.socialLoginFail({
+						id: '',
+						socialProvider,
+						profileImg: profileImge,
+						email: data.email,
+						goToSignUpPage: true,
+						code: 500,
+						username: ''
+					})
+				);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 };
 
-export const signUp = (username: string, email: string, socialProvider: string, profileImg: string) => {
-  return async (dispatch: any) => {
-    try {
-      const { data } = await axios({
-        method: "post",
-        url: `${process.env.REACT_APP_DOMAIN}/auth/register/social`,
-        data: {
-          email,
-          username,
-          profileImg,
-          socialProvider
-        }
-      });
-      localStorage.setItem("token", data.token);
-      dispatch(
-        actionCreators.signUpSuccess({
-          id: data.user.id,
-          username,
-          email,
-          profileImg,
-          socialProvider,
-          code: 200
-        })
-      );
-    } catch (error) {
-      if (error.response.status === 422 || error.status === 422) {
-        dispatch(
-          actionCreators.signUpFail({
-            id: "",
-            email,
-            code: 422,
-            profileImg,
-            socialProvider,
-            username
-          })
-        );
-      }
-    }
-  };
+export const signUp = (
+	username: string,
+	email: string,
+	socialProvider: string,
+	profileImg: string
+) => {
+	return async (dispatch: any) => {
+		try {
+			const { data } = await axios({
+				method: 'post',
+				url: `${process.env.REACT_APP_DOMAIN}/auth/register/social`,
+				data: {
+					email,
+					username,
+					profileImg,
+					socialProvider
+				}
+			});
+			localStorage.setItem('token', data.token);
+			dispatch(
+				actionCreators.signUpSuccess({
+					id: data.user.id,
+					username,
+					email,
+					profileImg,
+					socialProvider,
+					code: 200
+				})
+			);
+		} catch (error) {
+			if (error.response.status === 422 || error.status === 422) {
+				dispatch(
+					actionCreators.signUpFail({
+						id: '',
+						email,
+						code: 422,
+						profileImg,
+						socialProvider,
+						username
+					})
+				);
+			}
+		}
+	};
 };
 
 export const actionCreators = {
-  // 원래 작업은,
-  // const localLogin = (payload) =>{type:'user/LocalLogin', payload:payload}
-  socialLoginSuccess: createAction<IUserState>(SOCIAL_LOGIN_SUCCESS),
-  socialLoginFail: createAction<IUserState>(SOCIAL_LOGIN_FAIL),
-  fetchUserDataSuccess: createAction<IUserState>(FETCH_USER_DATA_SUCCESS),
-  fetchUserDataFail: createAction(FETCH_USER_DATA_FAIL),
-  signUpSuccess: createAction<IUserState>(SIGN_UP_SUCCESS),
-  signUpFail: createAction<IUserState>(SIGN_UP_FAIL),
-  logout: createAction(LOGOUT),
-  goToSignInPage: createAction(GO_TO_SIGN_IN_PAGE),
-  fetchUserData,
-  socialLoginAsync,
-  signUp
+	// 원래 작업은,
+	// const localLogin = (payload) =>{type:'user/LocalLogin', payload:payload}
+	socialLoginSuccess: createAction<IUserState>(SOCIAL_LOGIN_SUCCESS),
+	socialLoginFail: createAction<IUserState>(SOCIAL_LOGIN_FAIL),
+	fetchUserDataSuccess: createAction<IUserState>(FETCH_USER_DATA_SUCCESS),
+	fetchUserDataFail: createAction(FETCH_USER_DATA_FAIL),
+	signUpSuccess: createAction<IUserState>(SIGN_UP_SUCCESS),
+	signUpFail: createAction<IUserState>(SIGN_UP_FAIL),
+	logout: createAction(LOGOUT),
+	goToSignInPage: createAction(GO_TO_SIGN_IN_PAGE),
+	fetchUserData,
+	socialLoginAsync,
+	signUp
 };
 
 export interface IUserState {
-  id: string;
-  email: string;
-  username: string | null;
-  profileImg: string;
-  socialProvider: string;
-  message?: string;
-  goToSignUpPage?: boolean;
-  code: number | null;
+	id: string;
+	email: string;
+	username: string | null;
+	profileImg: string;
+	socialProvider: string;
+	message?: string;
+	goToSignUpPage?: boolean;
+	code: number | null;
 }
 
 const initialState: IUserState = {
-  id: "",
-  email: "",
-  username: "",
-  profileImg: "",
-  socialProvider: "",
-  goToSignUpPage: false,
-  code: null
+	id: '',
+	email: '',
+	username: '',
+	profileImg: '',
+	socialProvider: '',
+	goToSignUpPage: false,
+	code: null
 };
 
 // <A,B> A는 STATE의 타입이고, B는 payload의 타입
 export default handleActions<IUserState, any>(
-  {
-    [SOCIAL_LOGIN_SUCCESS]: (state, action): IUserState => {
-      const { id, email, username, profileImg, code, socialProvider } = action.payload;
-      return {
-        id,
-        email,
-        username,
-        profileImg,
-        socialProvider,
-        code
-      };
-    },
-    [SOCIAL_LOGIN_FAIL]: (state, action): IUserState => {
-      const { email, goToSignUpPage, profileImg, code, socialProvider } = action.payload;
-      return {
-        ...state,
-        email,
-        profileImg,
-        socialProvider,
-        goToSignUpPage,
-        code
-      };
-    },
-    [FETCH_USER_DATA_SUCCESS]: (state, action): IUserState => {
-      const { id, email, username, code, socialProvider, profileImg } = action.payload;
-      return {
-        ...state,
-        id,
-        email,
-        username,
-        profileImg,
-        socialProvider,
-        code
-      };
-    },
-    [FETCH_USER_DATA_FAIL]: (state, action): IUserState => {
-      return {
-        ...initialState
-      };
-    },
-    [GO_TO_SIGN_IN_PAGE]: (state, action): IUserState => {
-      return {
-        ...state,
-        goToSignUpPage: false
-      };
-    },
-    [SIGN_UP_SUCCESS]: (state, action): IUserState => {
-      const { id, email, username, code } = action.payload;
-      return {
-        ...state,
-        id,
-        username,
-        email,
-        code
-      };
-    },
-    [SIGN_UP_FAIL]: (state, action): IUserState => {
-      const { email, code } = action.payload;
-      return {
-        ...state,
-        email,
-        code
-      };
-    },
-    [LOGOUT]: (state, action): IUserState => {
-      localStorage.removeItem("token");
-      return {
-        ...initialState
-      };
-    }
-  },
-  initialState
+	{
+		[SOCIAL_LOGIN_SUCCESS]: (state, action): IUserState => {
+			const {
+				id,
+				email,
+				username,
+				profileImg,
+				code,
+				socialProvider
+			} = action.payload;
+			return {
+				id,
+				email,
+				username,
+				profileImg,
+				socialProvider,
+				code
+			};
+		},
+		[SOCIAL_LOGIN_FAIL]: (state, action): IUserState => {
+			const {
+				email,
+				goToSignUpPage,
+				profileImg,
+				code,
+				socialProvider
+			} = action.payload;
+			return {
+				...state,
+				email,
+				profileImg,
+				socialProvider,
+				goToSignUpPage,
+				code
+			};
+		},
+		[FETCH_USER_DATA_SUCCESS]: (state, action): IUserState => {
+			const {
+				id,
+				email,
+				username,
+				code,
+				socialProvider,
+				profileImg
+			} = action.payload;
+			return {
+				...state,
+				id,
+				email,
+				username,
+				profileImg,
+				socialProvider,
+				code
+			};
+		},
+		[FETCH_USER_DATA_FAIL]: (state, action): IUserState => {
+			return {
+				...initialState
+			};
+		},
+		[GO_TO_SIGN_IN_PAGE]: (state, action): IUserState => {
+			return {
+				...state,
+				goToSignUpPage: false
+			};
+		},
+		[SIGN_UP_SUCCESS]: (state, action): IUserState => {
+			const { id, email, username, code } = action.payload;
+			return {
+				...state,
+				id,
+				username,
+				email,
+				code
+			};
+		},
+		[SIGN_UP_FAIL]: (state, action): IUserState => {
+			const { email, code } = action.payload;
+			return {
+				...state,
+				email,
+				code
+			};
+		},
+		[LOGOUT]: (state, action): IUserState => {
+			localStorage.removeItem('token');
+			return {
+				...initialState
+			};
+		}
+	},
+	initialState
 );

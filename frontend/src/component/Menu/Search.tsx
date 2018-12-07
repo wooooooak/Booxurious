3aefbb3defcf515f8836ae09368816afdbcc65d9
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-12-07 23:55:29 +0900

    [#5] 유저 프로필 기능 보안
    - 메뉴바 이름 클릭시 링크 이동
    - 프로필 이름 속성 수정

--- frontend/src/component/Menu/Search.tsx
+++ frontend/src/component/Menu/Search.tsx
@@ -5,14 +5,14 @@ import { Search } from 'styled-icons/octicons/Search';
 // import { GoogleLogout } from 'react-google-login';
 
 interface IProps {
-  showInputBox: boolean;
-  username: string | null | undefined;
-  onClickSearchIcon(): void;
-  onClickSignIn(): void;
+	showInputBox: boolean;
+	username: string | null | undefined;
+	onClickSearchIcon(): void;
+	onClickSignIn(): void;
 }
 
 interface IState {
-  value: string;
+	value: string;
 }
 
 const SearchIcon = styled(Search)`
@@ -20,19 +20,19 @@ const SearchIcon = styled(Search)`
 `;
 
 const Contianer = styled.div`
-  margin-left: auto;
-  margin-right: 50px;
+	margin-left: auto;
+	margin-right: 50px;
 `;
 
 const SearchInput = styled.input`
-  margin-left: 20px;
-  margin-right: 20px;
-  width: 200px;
-  border-radius: 10px;
-  padding-left: 5px;
-  &:focus {
-    outline: none;
-  }
+	margin-left: 20px;
+	margin-right: 20px;
+	width: 200px;
+	border-radius: 10px;
+	padding-left: 5px;
+	&:focus {
+		outline: none;
+	}
 `;
 
 const StartText = styled(Link)`
@@ -42,48 +42,57 @@ const StartText = styled(Link)`
 `;
 
 class SearchForm extends React.Component<IProps, IState> {
-  state: IState = {
-    value: ''
-  };
+	state: IState = {
+		value: ''
+	};
 
-  onChangeInputValue = (e: React.FormEvent<HTMLInputElement>): void => {
-    const value: string = e.currentTarget.value;
-    this.setState({
-      value
-    });
-  };
+	onChangeInputValue = (e: React.FormEvent<HTMLInputElement>): void => {
+		const value: string = e.currentTarget.value;
+		this.setState({
+			value
+		});
+	};
 
-  onClickEnterInSearch = (e: React.KeyboardEvent): void => {
-    if (e.key === 'Enter') {
-      console.log('Enter');
-    }
-  };
+	onClickEnterInSearch = (e: React.KeyboardEvent): void => {
+		if (e.key === 'Enter') {
+			console.log('Enter');
+		}
+	};
 
-  render () {
-    const { showInputBox, onClickSearchIcon, username, onClickSignIn } = this.props;
-    return (
-      <Contianer>
-        {showInputBox ? (
-          <SearchInput
-            onChange={this.onChangeInputValue}
-            onKeyPress={this.onClickEnterInSearch}
-          />
-        ) : null}
-        <SearchIcon size="23" onClick={onClickSearchIcon} color="#011627" />
-        {username ? (
-          <React.Fragment>
-            <StartText to="/">Hi {username}!</StartText>
-            {/* <span style={{ color: 'black' }}>logout</span> */}
-            {/* <GoogleLogout buttonText="logout" onLogoutSuccess={this.logout} /> */}
-          </React.Fragment>
-        ) : (
-          <StartText to="/signIn" onClick={onClickSignIn}>
-            sign in
-          </StartText>
-        )}
-      </Contianer>
-    );
-  }
+	render() {
+		const {
+			showInputBox,
+			onClickSearchIcon,
+			username,
+			onClickSignIn
+		} = this.props;
+		return (
+			<Contianer>
+				{showInputBox ? (
+					<SearchInput
+						onChange={this.onChangeInputValue}
+						onKeyPress={this.onClickEnterInSearch}
+					/>
+				) : null}
+				<SearchIcon
+					size="23"
+					onClick={onClickSearchIcon}
+					color="#011627"
+				/>
+				{username ? (
+					<React.Fragment>
+						<StartText to={`/user/${username}`}>
+							Hi, {username}!
+						</StartText>
+					</React.Fragment>
+				) : (
+					<StartText to="/signIn" onClick={onClickSignIn}>
+						sign in
+					</StartText>
+				)}
+			</Contianer>
+		);
+	}
 }
 
 export default SearchForm;
--- frontend/src/component/Profile/InputForm.tsx
+++ frontend/src/component/Profile/InputForm.tsx
@@ -1,22 +1,27 @@
-import * as React from "react";
-import styled, { keyframes } from "styled-components";
+import * as React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-import { Avatar } from "antd";
+import { Avatar } from 'antd';
 
 const Layout = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 130px;
-  height: 450px;
-  background: #c6ffdd; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #f7797d, #fbd786, #c6ffdd); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to right,
-    #f7797d,
-    #fbd786,
-    #c6ffdd
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 130px;
+	height: 450px;
+	background: #c6ffdd; /* fallback for old browsers */
+	background: -webkit-linear-gradient(
+		to right,
+		#f7797d,
+		#fbd786,
+		#c6ffdd
+	); /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(
+		to right,
+		#f7797d,
+		#fbd786,
+		#c6ffdd
+	); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 
 const showUp = keyframes`
@@ -29,17 +34,16 @@ const showUp = keyframes`
 `;
 
 const CenterButton = styled.button`
-  width: 200px;
-  height: 60px;
-
-  position: relative;
-  top: 95px;
-  border-radius: 50px;
-  box-shadow: 1px 2px 30px grey;
-  border: none;
-  cursor: pointer;
-  opacity: 0;
-  animation: ${showUp} 0.2s linear 0.2s forwards;
+	width: 200px;
+	height: 60px;
+	position: relative;
+	top: 85px;
+	border-radius: 50px;
+	box-shadow: 1px 2px 30px grey;
+	border: none;
+	cursor: pointer;
+	opacity: 0;
+	animation: ${showUp} 0.2s linear 0.2s forwards;
 `;
 
 const FollowButton = styled(CenterButton)`
@@ -50,31 +54,40 @@ const SettingButton = styled(CenterButton)`
   background: pink;
 `;
 
+const UserName = styled.h2``;
+
 interface Props {
-  username: string | null;
-  profileImg: string;
-  isMe: boolean;
-  onClickSettingButton(): void;
+	username: string | null;
+	profileImg: string;
+	isMe: boolean;
+	onClickSettingButton(): void;
 }
 
-const InputForm: React.SFC<Props> = ({ username, profileImg, isMe, onClickSettingButton }) => {
-  if (isMe) {
-    return (
-      <Layout>
-        <Avatar size={160} icon="user" src={profileImg} />
-        <p>{username}</p>
-        <SettingButton onClick={() => onClickSettingButton()}>수정하기</SettingButton>
-      </Layout>
-    );
-  } else {
-    return (
-      <Layout>
-        <Avatar size={160} icon="user" src={profileImg} />
-        <p>{username}</p>
-        <FollowButton>팔로우하기</FollowButton>
-      </Layout>
-    );
-  }
+const InputForm: React.SFC<Props> = ({
+	username,
+	profileImg,
+	isMe,
+	onClickSettingButton
+}) => {
+	if (isMe) {
+		return (
+			<Layout>
+				<Avatar size={160} icon="user" src={profileImg} />
+				<UserName>{username}</UserName>
+				<SettingButton onClick={() => onClickSettingButton()}>
+					수정하기
+				</SettingButton>
+			</Layout>
+		);
+	} else {
+		return (
+			<Layout>
+				<Avatar size={160} icon="user" src={profileImg} />
+				<p>{username}</p>
+				<FollowButton>팔로우하기</FollowButton>
+			</Layout>
+		);
+	}
 };
 
 export default InputForm;
